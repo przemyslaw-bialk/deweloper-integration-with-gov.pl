666682a684b45aad9307035f31fb5896aed5da37
przemek <przemek.bialkwno@gmail.com>
2026-09-14 15:14:40 +0200

    rwd

--- components/(FRONTEND)/apartment/ApartmentFeatures.tsx
+++ components/(FRONTEND)/apartment/ApartmentFeatures.tsx
@@ -4,7 +4,7 @@ import { SlSettings } from "react-icons/sl";
 
 const ApartmentFeatures = () => {
   return (
-    <div className="flex gap-12 mt-6">
+    <div className="mt-10 grid grid-cols-2 bg-background-secondary p-4 md:bg-background md:p-0 md:flex md:gap-12 gap-6">
       <div className="flex flex-col items-center">
         <LiaCouchSolid className="text-accent text-xl md:text-4xl " />
         <p className="text-muted mt-2 tracking-wider text-sm text-center">
